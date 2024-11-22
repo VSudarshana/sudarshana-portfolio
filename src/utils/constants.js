a937754d56c67752d8f563cbc07557ed3b4ecfb3
VSudarshana <viswasudarshana@gmail.com>
2024-11-22 19:17:23 +0530

    updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,11 @@
 import project1 from "../assets/images/project1.png";
 import project2 from "../assets/images/project2.png";
 import project3 from "../assets/images/project3.png";
+import project4 from "../assets/images/project4.png";
+import project5 from "../assets/images/project5.png";
 
 export const RESUME_LINK =
-  "https://drive.google.com/file/d/1Ohw7cXiFM2bo120FkuGtfkg0xLXRTUFY/view?usp=sharing";
+  "https://drive.google.com/file/d/1Jx8SHwy_QAW8wAx4B-yq0GtJm6ffD673/view?usp=sharing";
 
 export const projects = [
   {
@@ -35,5 +37,26 @@ export const projects = [
       "Developed using React, this web application utilizes the OpenWeatherMap API to fetch real-time weather data, offering users a seamless experience for accessing weather information.",
     webUrl: "https://weather-react-smoky-one.vercel.app/",
     gitHubCode: "https://github.com/VSudarshana/weather-react",
+  },
+  {
+    title: "TODO-App using MERN Stack",
+    id: 4,
+    image: project4,
+    techStack: ["React Js", "Bootstrap", "NodeJS", "Express", "MongoDb"],
+    overView: `Developed a full-stack MERN application with robust RESTful APIs for creating, retrieving, updating, and deleting items, ensuring seamless backend functionality using Express.js and MongoDB.  
+Designed a dynamic React.js frontend with Bootstrap, enabling user-friendly interfaces for item management, including real-time listing and updates.  
+Resolved CORS policy issues, implemented error handling, and ensured secure CRUD operations for a reliable and responsive application.`,
+    webUrl: "https://github.com/VSudarshana/todo-mern",
+    gitHubCode: "https://github.com/VSudarshana/todo-mern",
+  },
+  {
+    title: "Bike service Application sing MERN",
+    id: 5,
+    image: project5,
+    techStack: ["React Js", "Bootstrap", "NodeJS", "Express", "MongoDb"],
+    overView: `MERN-Bike-App is a full-stack web application for managing bike services and bookings, built using the MERN (MongoDB, Express.js, React, Node.js) stack.
+    This project is a bike service station management app with an admin interface for managing bookings and service statuses and a user interface for customer registration, login, and bike service bookings.`,
+    webUrl: "https://github.com/VSudarshana/bikerService-MERN",
+    gitHubCode: "https://github.com/VSudarshana/bikerService-MERN",
   },
 ];
